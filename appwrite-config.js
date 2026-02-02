48ec1187835d9ea9250b8aab27b5f8b57ad696cc
pankajbca12101 <pankajbca12101@gmail.com>
2026-02-02 12:37:02 +0000

    add appp writer

---
+++ appwrite-config.js
@@ -0,0 +1,6 @@
+
+export const appwriteConfig = {
+    endpoint: "https://cloud.appwrite.io/v1", // Replace with your Appwrite endpoint
+    projectId: "698095fd002d2aba3f38", // Replace with your Appwrite Project ID
+    bucketId: "698096180017b7a9c9c4" // Replace with your Appwrite Storage Bucket ID
+};
---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyBd9xnnYBc0teWch8KinC8ku1noBkGPpm0",
+    authDomain: "royalintruders.firebaseapp.com",
+    projectId: "royalintruders",
+    storageBucket: "royalintruders.firebasestorage.app",
+    messagingSenderId: "417773145015",
+    appId: "1:417773145015:web:1d7edc6bd7e4318fbbb632",
+    measurementId: "G-GJ8GXG4BJW"
+};
